74f0d93c4b7aa1a0bb436896f909469b82b2d668
Ximdrake <asidorx@gmail.com>
2018-11-27 16:03:45 +0800

    maoni

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -102,9 +102,9 @@
     hours = hours % 12;
     hours = hours ? hours : 12;
     var strTime;
-    if(mins==04||mins==55){
-    	if(mins==04){
-        	strTime = (hours)+":0"+(mins)+ampm;
+    if(mins==25||mins==55){
+    	if(mins==25){
+        	strTime = (hours)+":"+(mins+5)+ampm;
         	console.log(strTime);
         }else if (mins==55&&hours==12){
         	strTime = (1)+":00"+ampm;
